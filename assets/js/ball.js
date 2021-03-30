2aa4852ee4d47772dd0abd97147153288fe94569
kir <34483316+NotToru@users.noreply.github.com>
2021-03-30 12:35:18 +0200

    ball & rectangle class

---
+++ assets/js/ball.js
@@ -0,0 +1,11 @@
+class Ball extends Rectangle {
+    constructor(x=0, y=0, color='white') {
+        super(x, y, 24, 24, color);
+        this.speed  = 0;
+        this.startX = x;
+        this.startY = y;
+        this.angle  = 0;
+        this.locked = false;
+        this.out    = false;
+    }
+}
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,11 @@
-class Game
-{
+class Game {
     constructor() {
+        // Verbinden met de ponggame-canvas (moet iedere frame opnieuw getekend worden)
+        this.canvas  = document.getElementById('pong');
+        this.context = this.canvas.getContext('2d');  
+
+        // Bal-object aanmaken
+        this.ball = new Ball(this.canvas.width/2, this.canvas.height/2, 'orange');
 
         // Gameloop
         let lastTime;
@@ -13,6 +18,8 @@ class Game
             window.requestAnimFrame(callback);
         }
         callback();
+
+        console.log(this.ball.size.y);
     }
 
     update(deltatime) {
@@ -20,6 +27,12 @@ class Game
     }
 
     draw() {
+        // Tekenen van de bal
+        this.drawRectangle(this.context, this.ball);
+    }
 
+    drawRectangle(ctx, rect, color='white') {
+        ctx.fillStyle = color;
+        ctx.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
     }
 }
