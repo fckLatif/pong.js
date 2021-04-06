2548009d8ff5b212f24d9d6a04ba295b2ae05ec9
kir <34483316+NotToru@users.noreply.github.com>
2021-04-06 12:38:00 +0200

    ball & simple collision

--- assets/js/ball.js
+++ assets/js/ball.js
@@ -1,5 +1,5 @@
 class Ball extends Rectangle {
-    constructor(x=0, y=0, color='white') {
+    constructor(x=0, y=0, color) {
         super(x, y, 24, 24, color);
         this.speed  = 0;
         this.startX = x;
@@ -7,5 +7,7 @@ class Ball extends Rectangle {
         this.angle  = 0;
         this.locked = false;
         this.out    = false;
+        this.velocity.x = (getRandomNumBetween(0,1)===1) ? getRandomNumBetween(-50,-150) : getRandomNumBetween(50,150);   
+        this.velocity.y = (getRandomNumBetween(0,1)===1) ? getRandomNumBetween(-50,-150) : getRandomNumBetween(50,150); 
     }
 }
